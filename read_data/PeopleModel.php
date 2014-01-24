21bfe8c7a9a0491b4a10ee7fc10abfd1d2ed0ddd
Anna Carlson <annacarlson@Annas-MacBook-Pro.local>
2014-01-23 16:03:25 -0800

    fixed add button

--- read_data/PeopleModel.php
+++ read_data/PeopleModel.php
@@ -70,7 +70,21 @@ public function logout(){
 public function update($id=0, $notesID='', $firstname='', $lastname='', $dob=''){
 	$sql = 'update userContacts set notesID = :notesID, firstname=:firstname, lastname=:lastname, dob=:dob where id=:id';
 	$st = $this->db->prepare($sql);
-	$st->execute(array(':id'=>$id, 'notesID'=>$notesID, ':firstname'=>$firstname, ':lastname'=>$lastname, ':dob'=>$dob));
+	$st->execute(array(':id'=>$id, ':notesID'=>$notesID, ':firstname'=>$firstname, ':lastname'=>$lastname, ':dob'=>$dob));
+
+}
+public function delete($id=0){
+	$sql='delete from userContacts where userID = :userID';
+	$st = $this->db->prepare($sql);
+	$st->execute(array(':userID'=>$id));
+	
+}
+
+public function add($notesID='', $firstname='', $lastname='', $dob=''){
+	$sql= 'insert into userContacts(firstname, lastname, dob, notesID)
+						values (:firstname, :lastname, :dob, :notesID)';
+	$st = $this->db->prepare($sql);
+	$st-> execute(array('notesID'=>$notesID, ':firstname'=>$firstname, ':lastname'=>$lastname, ':dob'=>$dob));
 
 }
 	
