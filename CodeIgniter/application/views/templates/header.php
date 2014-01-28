a7be9db07ae0c14b5c6e7432fb5538b5ec386e8c
Anna Carlson <annacarlson@Annas-MacBook-Pro.local>
2014-01-27 21:24:34 -0800

    css

--- CodeIgniter/application/views/templates/header.php
+++ CodeIgniter/application/views/templates/header.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title><?php echo $title ?> - Frameworks tutorial</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo LAYOUT_URL;?>style1.css">
 </head>
 <body>
 	<h1>Anna Carlson - frameworks</h1>
